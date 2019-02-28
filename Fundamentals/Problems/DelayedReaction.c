afd4dfda06a49a3a58613ff26b04ac44cb99e6e6
ChipooKing <teckwee2014@gmail.com>
2019-02-28 23:25:07 +0800

    added gitignore

--- Fundamentals/Problems/DelayedReaction.c
+++ Fundamentals/Problems/DelayedReaction.c
@@ -1,3 +1,37 @@
 /*  PROBLEM: Write a program that prints a string to the console when a user inputs something, but only after a delay of
     5 seconds.
 */
+#include <stdio.h>
+#include <time.h>
+
+void delay(int seconds);
+
+int main(int argc, char const *argv[])
+{
+    char input[50];
+    int i = 1;
+
+    do
+    {
+        printf("Input something: ");
+        scanf("%49s", &input);
+
+        delay(5);
+
+        printf("May the Force be with you...\n\n");
+    } while (i == 1);
+
+    return 0;
+}
+
+void delay(int seconds)
+{
+    // Converting time into milli_seconds 
+    int milli_seconds = 1000 * seconds; 
+  
+    // Storing current number of clock ticks since start of program
+    clock_t start_time = clock(); 
+  
+    // looping till number of milliseconds from starting clock ticks is reached
+    while (clock() < start_time + milli_seconds);
+}
--- Fundamentals/Problems/SearchString.c
+++ Fundamentals/Problems/SearchString.c
@@ -1,3 +1,30 @@
 /*  PROBLEM: Search a user input string for a specific word.
     E.g. "There was an apple in the yard.", check if "apple" is in the string.
 */
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char const *argv[])
+{
+    char input[100];
+    char key[10] = "apple";
+
+    printf("Input a string to search: ");
+    fgets(input, 100, stdin);
+
+
+    // convert all characters to lowercase before comparing
+    for (int i=0; input[i];i++) {
+        input[i] = tolower(input[i]);
+    }
+
+    if (strstr(input, key) != NULL) {
+        printf("apple found!");
+    }
+    else {
+        printf("apple not found!");
+    }
+
+    return 0;
+}
